97ff80d39ba8ead7cef10cfdd3799d9595b6241f
Froxzen <harvardchong1@gmail.com>
2024-09-13 12:18:58 +1000

    Added prediction plot graph

--- predict.py
+++ predict.py
@@ -14,3 +14,21 @@ plt.title('House Prices vs House Size')
 plt.xlabel('House Size (sq.ft)')
 plt.ylabel('House Price ($)')
 plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(house_sizes, house_prices, test_size=0.2, random_state=42)
+
+X_train = X_train.reshape(-1, 1)
+X_test = X_test.reshape(-1, 1)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+plt.scatter(X_test, y_test, marker='o', color='blue', label='Actual Prices')
+plt.plot(X_test, predictions, color='red', linewidth=2, label='Predicted Prices')
+plt.title('Property Price Prediction with Linear Regression')
+plt.xlabel('House Size (sq.ft)')
+plt.ylabel('House Price (millions $)')
+plt.legend()
+plt.show()
