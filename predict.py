b5d713e1f4a4750ee559647f158fd550ac98cc05
Froxzen <harvardchong1@gmail.com>
2024-09-13 13:04:21 +1000

    Multiple Graphs can now be displayed on a single window

--- predict.py
+++ predict.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -9,11 +8,13 @@ data = pd.read_csv('home_dataset.csv')
 house_sizes = data['HouseSize'].values
 house_prices = data['HousePrice'].values
 
-plt.scatter(house_sizes, house_prices, marker = 'o', color = 'blue')
-plt.title('House Prices vs House Size')
-plt.xlabel('House Size (sq.ft)')
-plt.ylabel('House Price ($)')
-plt.show()
+fig, axis = plt.subplots(1, 2, figsize=(12, 6))
+
+# Plotting original data
+axis[0].scatter(house_sizes, house_prices, marker = 'o', color = 'blue')
+axis[0].set_title('House Prices vs House Size')
+axis[0].set_xlabel('House Size (sq.ft)')
+axis[0].set_ylabel('House Price ($)')
 
 X_train, X_test, y_train, y_test = train_test_split(house_sizes, house_prices, test_size=0.2, random_state=42)
 
@@ -25,10 +26,13 @@ model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
 
-plt.scatter(X_test, y_test, marker='o', color='blue', label='Actual Prices')
-plt.plot(X_test, predictions, color='red', linewidth=2, label='Predicted Prices')
-plt.title('Property Price Prediction with Linear Regression')
-plt.xlabel('House Size (sq.ft)')
-plt.ylabel('House Price (millions $)')
-plt.legend()
+# Plotting data with prediction
+axis[1].scatter(X_test, y_test, marker='o', color='blue', label='Actual Prices')
+axis[1].plot(X_test, predictions, color='red', linewidth=2, label='Predicted Prices')
+axis[1].set_title('Property Price Prediction with Linear Regression')
+axis[1].set_xlabel('House Size (sq.ft)')
+axis[1].set_ylabel('House Price (millions $)')
+axis[1].legend()
+
+plt.tight_layout()
 plt.show()
